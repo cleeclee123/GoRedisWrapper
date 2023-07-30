51229ebfe495c22c0a63dbcd4a58e59b35e8426c
Chris Lee <cl114@illinois.edu>
2023-07-30 15:17:27 -0400

    format

--- main.go
+++ main.go
@@ -1,95 +1,95 @@
 package main
 
 import (
-  "context"
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
-  "github.com/joho/godotenv"
 	"net/http"
-  "fmt"
-  "os"
+	"os"
 )
 
 func main() {
-  client := redis.NewClient(&redis.Options{
-    Addr: goDotEnv("REDIS_ADDY"),
-    Password: goDotEnv("REDIS_PW"), 
-    DB:       0,  
-  })
-  ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr:     goDotEnv("REDIS_ADDY"),
+		Password: goDotEnv("REDIS_PW"),
+		DB:       0,
+	})
+	ctx := context.Background()
 
-  err := client.Ping(ctx).Err()
-  if err != nil {
-    panic("redis client connect failed")
-  }
+	err := client.Ping(ctx).Err()
+	if err != nil {
+		panic("redis client connect failed")
+	}
 
 	router := gin.Default()
-  router.GET("/:apikey", home)
-  router.GET("/set/:key/:value/:apikey", setRedis(ctx, client))
-  router.GET("/get/:key/:apikey", getRedis(ctx, client))
+	router.GET("/:apikey", home)
+	router.GET("/set/:key/:value/:apikey", setRedis(ctx, client))
+	router.GET("/get/:key/:apikey", getRedis(ctx, client))
 
 	router.Run("localhost:8080")
 }
 
 func goDotEnv(key string) string {
-  err := godotenv.Load(".env")
-  if err != nil {
-    panic("env load failed")
-  }
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("env load failed")
+	}
 
-  return os.Getenv(key)
+	return os.Getenv(key)
 }
 
 func home(c *gin.Context) {
-  apikey := c.Param("apikey")
+	apikey := c.Param("apikey")
 
-  if (apikey != goDotEnv("API_KEY")) {
-    c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
-    return
-  }
-  c.IndentedJSON(http.StatusNotFound, gin.H{"hello": "welcome to my basic redis rest wrapper"})
+	if apikey != goDotEnv("API_KEY") {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
+		return
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"hello": "welcome to my basic redis rest wrapper"})
 }
 
 func setRedis(ctx context.Context, client *redis.Client) gin.HandlerFunc {
-  fn := func(c *gin.Context) {
-    key := c.Param("key")
-    value := c.Param("value")
-    apikey := c.Param("apikey")
+	fn := func(c *gin.Context) {
+		key := c.Param("key")
+		value := c.Param("value")
+		apikey := c.Param("apikey")
 
-    if (apikey != goDotEnv("API_KEY")) {
-      c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
-      return
-    }
-    
-    err := client.Set(ctx, key, value, 0).Err()
-    if err != nil {
-      c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
-      return
-    }
-    c.IndentedJSON(http.StatusOK, gin.H{"success": 200})
-  }
+		if apikey != goDotEnv("API_KEY") {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
+			return
+		}
 
-  return gin.HandlerFunc(fn)
+		err := client.Set(ctx, key, value, 0).Err()
+		if err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		c.IndentedJSON(http.StatusOK, gin.H{"success": 200})
+	}
+
+	return gin.HandlerFunc(fn)
 }
 
 func getRedis(ctx context.Context, client *redis.Client) gin.HandlerFunc {
-  fn := func(c *gin.Context) {
-    key := c.Param("key")
-    apikey := c.Param("apikey") 
-    
-    if (apikey != goDotEnv("API_KEY")) {
-      c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
-      return
-    }
-    
-    val, err := client.Get(ctx, key).Result()
-    fmt.Println("value", val)
-    if err != nil {
-      c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
-      return
-    }
-    c.IndentedJSON(http.StatusOK, gin.H{key: val})
-  }
-  
-  return gin.HandlerFunc(fn)
+	fn := func(c *gin.Context) {
+		key := c.Param("key")
+		apikey := c.Param("apikey")
+
+		if apikey != goDotEnv("API_KEY") {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "bad api key"})
+			return
+		}
+
+		val, err := client.Get(ctx, key).Result()
+		fmt.Println("value", val)
+		if err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		c.IndentedJSON(http.StatusOK, gin.H{key: val})
+	}
+
+	return gin.HandlerFunc(fn)
 }
